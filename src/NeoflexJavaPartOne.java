634c3bbb218b76d4c3e6cb707c622fabbfaa10e6
yastrebow <yastrebow@gmail.com>
2021-11-20 13:19:54 +0300

    Решение домашнего задания (часть 1) к лекции по Java от 10.11.2021

--- src/NeoflexJavaPartOne.java
+++ src/NeoflexJavaPartOne.java
@@ -81,12 +81,11 @@ public class NeoflexJavaPartOne {
         System.out.println("Привет, " + name + "!");
     }
 
-
     //8. * Написать метод, который определяет, является ли год високосным, и выводит сообщение в консоль.
 // Каждый 4-й год является високосным, кроме каждого 100-го, при этом каждый 400-й – високосный.
     static void leapYear(int year) {
         if (!(year % 4 == 0) || (year % 100 == 0) && (!(year % 400 == 0))) {
-            System.out.println(year + " год не является високосным");
+            System.out.println(year + " год не високосный");
         } else {
             System.out.println(year + " год - високосный");
         }
